00d9512a689f23b2fda090d78b09777472ef7e7e
Van <lvanlowe@inrvisions.com>
2019-09-19 17:26:21 -0400

    Add Event Hub Messages

---
+++ SendEventHubMessages/SendEventHubMessages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SendEventHubMessages
+{
+    class Program
+    {
+
+        private static EventHubClient eventHubClient;
+        private const string EventHubConnectionString = "{Event Hubs connection string}";
+        private const string EventHubName = "{Event Hub path/name}";
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var conn = configuration.GetConnectionString("Storage");
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+
+        }
+    }
+}
