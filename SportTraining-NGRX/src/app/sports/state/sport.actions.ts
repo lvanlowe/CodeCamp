dc9d03ff09b7919eeaf4c428eb97a3c43c9dd6e3
Van <lvanlowe@inrvisions.com>
2019-10-01 22:06:48 -0400

    Add sports actions

--- SportTraining-NGRX/src/app/sports/state/sport.actions.ts
+++ SportTraining-NGRX/src/app/sports/state/sport.actions.ts
@@ -1,14 +1,76 @@
 import { Action } from '@ngrx/store';
+import { Sport } from 'src/app/sport';
 
 export enum SportActionTypes {
   LoadSports = '[Sport] Load Sports',
-  
-  
+  LoadSportSuccess = '[Sport] Load Sport Success',
+  LoadSportFail = '[Sport] Load Sport Fail',
+  UpdateSport = '[Sport] Update Sport',
+  UpdateSportSuccess = '[Sport] Update Sport Success',
+  UpdateSportFail = '[Sport] Update Sport Fail',
+  GetSport = '[Sport] Get Sport',
+  GetSportSuccess = '[Sport] Get Sport Success',
+  GetSportFail = '[Sport] Get Sport Fail',
 }
 
 export class LoadSports implements Action {
   readonly type = SportActionTypes.LoadSports;
 }
 
+export class LoadSportSuccess implements Action {
+  readonly type = SportActionTypes.LoadSportSuccess;
 
-export type SportActions = LoadSports;
+  constructor(public payload: Sport[]) {}
+}
+
+export class LoadSportFail implements Action {
+  readonly type = SportActionTypes.LoadSportFail;
+
+  constructor(public payload: string) {}
+}
+
+export class UpdateSport implements Action {
+  readonly type = SportActionTypes.UpdateSport;
+
+  constructor(public payload: Sport) {}
+}
+
+export class UpdateSportSuccess implements Action {
+  readonly type = SportActionTypes.UpdateSportSuccess;
+
+}
+
+export class UpdateSportFail implements Action {
+  readonly type = SportActionTypes.UpdateSportFail;
+
+  constructor(public payload: string) {}
+}
+export class GetSport implements Action {
+  readonly type = SportActionTypes.GetSport;
+
+  constructor(public payload: number) {}
+}
+
+export class GetSportSuccess implements Action {
+  readonly type = SportActionTypes.GetSportSuccess;
+
+  constructor(public payload: Sport) {}
+}
+
+export class GetSportFail implements Action {
+  readonly type = SportActionTypes.GetSportFail;
+
+  constructor(public payload: string) {}
+}
+
+
+export type SportActions = LoadSports
+| LoadSportSuccess
+| LoadSportFail
+| UpdateSport
+| UpdateSportSuccess
+| UpdateSportFail
+| GetSport
+| GetSportSuccess
+| GetSportFail;
+
