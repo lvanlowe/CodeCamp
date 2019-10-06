cefeb3e5aa008087d19ca2cb79506cf95bd08ec0
Van <lvanlowe@inrvisions.com>
2019-10-06 15:46:02 -0400

    update location effects

--- SportTraining-NGRX/src/app/sports/locations-details/state/location.effects.ts
+++ SportTraining-NGRX/src/app/sports/locations-details/state/location.effects.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
-import { concatMap } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
-import { LocationActionTypes, LocationActions } from './location.actions';
-
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import * as locationActions from './location.actions';
+import { LocationService } from '../../service/location.service';
+import { Action } from '@ngrx/store';
+import { Program } from 'src/app/location';
 
 
 @Injectable()
 export class LocationEffects {
 
+  constructor(private locationService: LocationService, private actions$: Actions) {}
 
   @Effect()
-  loadLocations$ = this.actions$.pipe(
-    ofType(LocationActionTypes.LoadLocations),
-    /** An EMPTY observable only emits completion. Replace with your own observable API request */
-    concatMap(() => EMPTY)
+  loadLocationsBySport$: Observable<Action> = this.actions$.pipe(
+    ofType(locationActions.LocationActionTypes.LoadLocations),
+    map((action: locationActions.LoadLocations) => action.payload),
+    mergeMap((sportid: number) =>
+      this.locationService.getLocationsBySport(sportid).pipe(
+        map(locations => new locationActions.LoadLocationSuccess(locations)),
+        catchError(err => of(new locationActions.LoadLocationFail(err)))
+      )
+    )
   );
 
-
-  constructor(private actions$: Actions<LocationActions>) {}
-
+  @Effect()
+  updateLocations$: Observable<Action> = this.actions$.pipe(
+    ofType(locationActions.LocationActionTypes.UpdateLocation),
+    map((action: locationActions.UpdateLocation) => action.payload),
+    mergeMap((location: Program) =>
+      this.locationService.updateLocation(location).pipe(
+        map(locations => new locationActions.UpdateLocationSuccess()),
+        catchError(err => of(new locationActions.UpdateLocationFail(err)))
+      )
+    )
+  );
 }
--- SportTraining-NGRX/src/app/sports/state/sport.effects.ts
+++ SportTraining-NGRX/src/app/sports/state/sport.effects.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
-import { concatMap, map, mergeMap, catchError } from 'rxjs/operators';
-import { EMPTY, Observable, of } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import * as sportActions from './sport.actions';
 import { Action } from '@ngrx/store';
 import { SportService } from '../service/sport.service';
 import { Sport } from 'src/app/sport';
-// import { Action } from 'rxjs/internal/scheduler/Action';
+
 
 
 
@@ -15,14 +15,6 @@ import { Sport } from 'src/app/sport';
 export class SportEffects {
 
   constructor(private sportService: SportService, private actions$: Actions) {}
-
-  // @Effect()
-  // loadSports$ = this.actions$.pipe(
-  //   ofType(SportActionTypes.LoadSports),
-  //   /** An EMPTY observable only emits completion. Replace with your own observable API request */
-  //   concatMap(() => EMPTY)
-  // );
-
 
   @Effect()
   loadSports$: Observable<Action> = this.actions$.pipe(
