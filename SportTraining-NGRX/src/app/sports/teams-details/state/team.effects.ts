1c4d2046a20dae4541a4fb3359b919f198aa1915
Van <lvanlowe@inrvisions.com>
2019-10-06 18:01:08 -0400

    update effects and reducer

--- SportTraining-NGRX/src/app/sports/teams-details/state/team.effects.ts
+++ SportTraining-NGRX/src/app/sports/teams-details/state/team.effects.ts
@@ -1,24 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 
-import { concatMap } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
-import { TeamActionTypes, TeamActions } from './team.actions';
+import {  map, mergeMap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import * as teamActions from './team.actions';
+import { TeamService } from '../../service/team.service';
+import { Action } from '@ngrx/store';
+import { Team } from 'src/app/team';
 
 
 
 @Injectable()
 export class TeamEffects {
 
+  constructor(private teamService: TeamService, private actions$: Actions) {}
 
   @Effect()
-  loadTeams$ = this.actions$.pipe(
-    ofType(TeamActionTypes.LoadTeams),
-    /** An EMPTY observable only emits completion. Replace with your own observable API request */
-    concatMap(() => EMPTY)
+  loadTeamsByLocation$: Observable<Action> = this.actions$.pipe(
+    ofType(teamActions.TeamActionTypes.LoadTeamsLocation),
+    map((action: teamActions.LoadTeamsLocation) => action.payload),
+    mergeMap((locationid: number) =>
+      this.teamService.getTeamsByLocation(locationid).pipe(
+        map(teams => new teamActions.LoadTeamSuccess(teams)),
+        catchError(err => of(new teamActions.LoadTeamFail(err)))
+      )
+    )
   );
 
+  @Effect()
+  loadTeamsByCategory$: Observable<Action> = this.actions$.pipe(
+    ofType(teamActions.TeamActionTypes.LoadTeamsCategory),
+    map((action: teamActions.LoadTeamsCategory) => action.payload),
+    mergeMap((categoryid: number) =>
+      this.teamService.getTeamsByCategory(categoryid).pipe(
+        map(teams => new teamActions.LoadTeamSuccess(teams)),
+        catchError(err => of(new teamActions.LoadTeamFail(err)))
+      )
+    )
+  );
 
-  constructor(private actions$: Actions<TeamActions>) {}
+  @Effect()
+  updateTeams$: Observable<Action> = this.actions$.pipe(
+    ofType(teamActions.TeamActionTypes.UpdateTeam),
+    map((action: teamActions.UpdateTeam) => action.payload),
+    mergeMap((team: Team) =>
+      this.teamService.updateTeam(team).pipe(
+        map(teams => new teamActions.UpdateTeamSuccess()),
+        catchError(err => of(new teamActions.UpdateTeamFail(err)))
+      )
+    )
+  );
 
 }
