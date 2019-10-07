c333434d8e148a7b4fc1b1f94baeec839500db66
Van <lvanlowe@inrvisions.com>
2019-10-07 19:30:00 -0400

    update sports action

--- SportTraining-NGRX/src/app/sports/state/sport.actions.ts
+++ SportTraining-NGRX/src/app/sports/state/sport.actions.ts
@@ -1,17 +1,6 @@
-import { Action, createAction, union } from '@ngrx/store';
+import { createAction, union } from '@ngrx/store';
 import { Sport } from 'src/app/sport';
 
-// export enum SportActionTypes {
-//   LoadSports = '[Sport] Load Sports',
-//   LoadSportSuccess = '[Sport] Load Sport Success',
-//   LoadSportFail = '[Sport] Load Sport Fail',
-//   UpdateSport = '[Sport] Update Sport',
-//   UpdateSportSuccess = '[Sport] Update Sport Success',
-//   UpdateSportFail = '[Sport] Update Sport Fail',
-//   GetSport = '[Sport] Get Sport',
-//   GetSportSuccess = '[Sport] Get Sport Success',
-//   GetSportFail = '[Sport] Get Sport Fail',
-// }
 
 export const LoadSports  = createAction(
   '[Sport] Load Sports',
@@ -28,71 +17,48 @@ export const LoadSportFail = createAction(
   (payload: { error: string }) => ({ payload })
 );
 
-// export class LoadSports implements Action {
-//   readonly type = SportActionTypes.LoadSports;
-// }
+export const UpdateSports  = createAction(
+  '[Sport] Update Sports',
+  (payload: { sport: Sport }) => ({ payload })
+);
 
-// export class LoadSportSuccess implements Action {
-//   readonly type = SportActionTypes.LoadSportSuccess;
+export const UpdateSportSuccess  = createAction(
+  '[Sport] Update Sport Success',
+  (payload: { }) => ({ payload })
+);
 
-//   constructor(public payload: Sport[]) {}
-// }
+export const UpdateSportFail = createAction(
+  '[Sport] Update Sport Fail',
+  (payload: { error: string }) => ({ payload })
+);
 
-// export class LoadSportFail implements Action {
-//   readonly type = SportActionTypes.LoadSportFail;
+export const GetSport  = createAction(
+  '[Sport] Get Sport',
+  (payload: { sportid: number }) => ({ payload })
+);
 
-//   constructor(public payload: string) {}
-// }
+export const GetSportSuccess  = createAction(
+  '[Sport] Get Sport Success',
+  (payload: { sport: Sport }) => ({ payload })
+);
 
-// export class UpdateSport implements Action {
-//   readonly type = SportActionTypes.UpdateSport;
-
-//   constructor(public payload: Sport) {}
-// }
-
-// export class UpdateSportSuccess implements Action {
-//   readonly type = SportActionTypes.UpdateSportSuccess;
-
-// }
-
-// export class UpdateSportFail implements Action {
-//   readonly type = SportActionTypes.UpdateSportFail;
-
-//   constructor(public payload: string) {}
-// }
-// export class GetSport implements Action {
-//   readonly type = SportActionTypes.GetSport;
-
-//   constructor(public payload: number) {}
-// }
-
-// export class GetSportSuccess implements Action {
-//   readonly type = SportActionTypes.GetSportSuccess;
-
-//   constructor(public payload: Sport) {}
-// }
-
-// export class GetSportFail implements Action {
-//   readonly type = SportActionTypes.GetSportFail;
-
-//   constructor(public payload: string) {}
-// }
+export const GetSportFail = createAction(
+  '[Sport] Get Sport Fail',
+  (payload: { error: string }) => ({ payload })
+);
 
 
-// export type SportActions = LoadSports
-// | LoadSportSuccess
-// | LoadSportFail
-// | UpdateSport
-// | UpdateSportSuccess
-// | UpdateSportFail
-// | GetSport
-// | GetSportSuccess
-// | GetSportFail;
 
 const SportActions = union({
   LoadSports,
   LoadSportSuccess,
   LoadSportFail,
+  UpdateSports,
+  UpdateSportSuccess,
+  UpdateSportFail,
+  GetSport,
+  GetSportSuccess,
+  GetSportFail
 })
 
 export type SportUnion = typeof SportActions;
